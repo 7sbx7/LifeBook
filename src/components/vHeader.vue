02a18254d756304c5b794f300a5ffe6243976d04
Clearmedia <clearmedia@MacBook-Pro-Sebastian.local>
2023-10-04 08:46:59 +0200

    feat: files cleanup, add global scss

---
+++ src/components/vHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navigation">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'vHeader'
+});
+</script>
+
+<style lang="sass" scoped>
+  .navigation
+    display: flex
+    justify-content: center
+    width: 100%
+    border: 1px solid red
+    height: 40px
+</style>

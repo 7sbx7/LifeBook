a5fccee2a8c3aa8a61042838abe7758d079fb835
Sebastian <sbx@MacBook-Pro-Sebastian.local>
2023-11-29 06:56:23 +0100

    feat(store): add separate getters, actions, mutations files

---
+++ src/store/mutations.ts
@@ -0,0 +1,9 @@
+export default {
+  SET_LOGGED_IN(state, value) {
+    state.user.loggedIn = value
+  },
+  SET_USER(state, data) {
+    state.user.data = data
+    state.user.loggedIn = data !== null
+  },
+};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">home</div>
+  <button @click="showUser">Show user</button>
   <button @click.prevent="handleLogout">Log out</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, onMounted } from 'vue';
 import { useAuth } from '@/composables/useAuth';
 import { getAuth } from 'firebase/auth';
+import { useStore } from 'vuex';
 
 export default defineComponent({
   name: 'HomeView',
@@ -16,8 +18,21 @@ export default defineComponent({
     const auth = getAuth();
     const handleLogout = async () => useAuth(auth).handleLogout()
 
+    const store = useStore();
+
+    onMounted(() => {
+      store.dispatch('user/fetchCurrentUser');
+    });
+    
+    const currentUser = computed(() => store.getters['user/currentUser']);
+
+    const showUser = () => {
+      console.log(currentUser);
+    };
+
     return {
-      handleLogout
+      handleLogout,
+      showUser
     }
   }
 });
